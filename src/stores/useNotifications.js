1a1f48a8acfc19ab28a6a836cbb9ca0de9921d41
hpcao299 <caohoangphuc2909@gmail.com>
2025-07-07 14:39:14 +0700

    fix initial state

--- src/stores/useNotifications.js
+++ src/stores/useNotifications.js
@@ -2,13 +2,7 @@ import { create } from 'zustand';
 
 // Zustand store for managing notifications
 const useNotifications = create(set => ({
-    notifications: [
-        {
-            id: Date.now(),
-            title: 'THÊM MÓN THÀNH CÔNG',
-            message: 'Super Sundae Socolaa x1',
-        },
-    ],
+    notifications: [],
 
     addNotification(title, message, type) {
         const id = Date.now();
